6820ad0702cce6eff1913fa705ad4645cc1678d4
jonas <jonas_mo90@hotmail.com>
2017-07-01 23:51:18 +0200

    files

---
+++ index.js
@@ -0,0 +1,48 @@
+var images = ['old','young','nature']
+var count = images.length
+
+images = images.map(function (image){
+  var img =  new Image()
+  img.onload = function () {
+    next()
+  }
+  img.src = image + '.jpg'
+  return img
+})
+
+function next () {
+  count--
+  if (count < 1) {
+    images.forEach (function (image){
+      document.body.appendChild(image)
+    })
+  }
+}
+
+/*
+var fs = require('fs')
+
+function getImg(filepath, done){
+  fs.readFile(filepath, function (err, imgs) {
+    if(err) return done(err)
+
+    fs.readFile('imgs.dictionary', function(err, dict) {
+      if(err) return done(err)
+
+      compareImg(imgs, dict)
+    })
+  })
+
+  function compareImg (imgs, dict) {
+    dict = dict.toString().split('\n')
+    imgs = imgs.toString().split('\n').filter(function (img) {
+      return dict.indexOf(img) !== -1
+    })
+    done (null, imgs)
+  }
+}
+
+getImg('imgs.txt', function (err, imgs) {
+  console.log(imgs)
+})
+*/
